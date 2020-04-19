1e5d4abaf99e59e5bf55dd3bb01daba11b9081a2
9831120 <hashemiamirreza@hotmail.com>
2020-04-19 20:43:53 +0430

    Person added

---
+++ AmirrezaHashemi/Lab4/src/Person.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Person {
+    private String fistName;
+    private String lastName;
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return fistName+' '+lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return fistName.equals(person.fistName) &&
+                lastName.equals(person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fistName, lastName);
+    }
+}
