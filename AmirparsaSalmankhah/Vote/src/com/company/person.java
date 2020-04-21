17616abeda79e064ad61a16114b5d5464b9cf5a8
Amirparsa-Sal <Amirparsa.s@aut.ac.ir>
2020-04-21 20:50:22 +0430

    person class added

---
+++ AmirparsaSalmankhah/Vote/src/com/company/person.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class person {
+    private String firstName;
+    private String lastName;
+
+    public person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
