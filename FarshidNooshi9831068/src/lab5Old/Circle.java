ff9855d96eabc69f5b64237ca63b79d124be95d5
FarshidNooshi@users.noreply.github.com <FarshidNooshi@users.noreply.github.com>
2020-04-24 22:57:17 +0430

    old version is done

--- FarshidNooshi9831068/src/lab5Old/Circle.java
+++ FarshidNooshi9831068/src/lab5Old/Circle.java
@@ -1,4 +1,74 @@
+//In The Name Of GOD
+
 package lab5Old;
 
-public class Circle {
+import java.util.Objects;
+
+public class Circle extends Paint {
+    private static final double pi = 3.14159265;
+    private final double radius;
+
+    /**
+     * a constructor for our class
+     *
+     * @param radius is the radius of the circle
+     */
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * this method just draws our shape in the console
+     */
+    public void draw() {
+        System.out.println("Circle with " + calculateArena() + " arena and " + calculatePerimeter() + " perimeter.");
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * @return the arena of the shape
+     */
+    public double calculateArena() {
+        return pi * radius * radius;
+    }
+
+    /**
+     * @return the perimeter of the shape
+     */
+    public double calculatePerimeter() {
+        return 2 * pi * radius;
+    }
+
+    /**
+     * @param obj is the object to be checked
+     * @return true if the shapes are equal
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != getClass())
+            return false;
+        Circle tmp = (Circle) obj;
+        return tmp.getRadius() == radius;
+    }
+
+    /**
+     * @return the hash value of the shape depending on its properties
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    /**
+     * @return an string showing the properties of the shape
+     */
+    @Override
+    public String toString() {
+        return "Circle and radius equals to " + radius;
+    }
 }
--- FarshidNooshi9831068/src/lab5Old/Triangle.java
+++ FarshidNooshi9831068/src/lab5Old/Triangle.java
@@ -1,5 +1,98 @@
+//In The Name Of GOD
+
 package lab5Old;
 
-public class Triangle {
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.TransferQueue;
 
+public class Triangle extends Paint {
+    private ArrayList<Double> sides;
+
+    /**
+     * its a constructor for our class
+     *
+     * @param s1 is a side of our Triangle
+     * @param s2 is a side of our Triangle
+     * @param s3 is a side of our Triangle
+     */
+    public Triangle(double s1, double s2, double s3) {
+        sides = new ArrayList<>();
+        sides.add(s1);
+        sides.add(s2);
+        sides.add(s3);
+    }
+
+    public ArrayList<Double> getSides() {
+        return sides;
+    }
+
+    /**
+     * @return true if the triangle is equilateral
+     */
+    public boolean isEquilateral() {
+        return sides.get(0) == sides.get(1) && sides.get(1) == sides.get(2);
+    }
+
+    /**
+     * @return the arena of the shape with the heron formula
+     */
+    public double calculateArena() {
+        double p = calculatePerimeter() / 2;
+        return Math.sqrt(p * (p - sides.get(0)) * (p - sides.get(1)) * (p - sides.get(2)));
+    }
+
+    /**
+     * @return the perimeter of the shape
+     */
+    public double calculatePerimeter() {
+        double ret = 0;
+        for (Double it : sides)
+            ret += it;
+        return ret;
+    }
+
+    /**
+     * this method draws our shape
+     */
+    public void draw() {
+        System.out.print("Triangle with " + calculateArena() + " arena and " + calculatePerimeter() + " perimeter");
+        if (isEquilateral())
+            System.out.println(" its equilateral.");
+        else
+            System.out.println(".");
+    }
+
+    /**
+     * @param obj is the object to be checked
+     * @return true if the shapes are equal
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != getClass())
+            return false;
+        Rectangle tmp = (Rectangle) obj;
+        for (int i = 0; i < 3; i++)
+            if (!sides.get(i).equals(tmp.getSides().get(i)))
+                return false;
+        return true;
+    }
+
+    /**
+     * @return the hash value of the shape depending on its properties
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(sides);
+    }
+
+    /**
+     * @return an string showing the properties of the shape
+     */
+    @Override
+    public String toString() {
+        return "Triangle and sides equal to " + sides.get(0) + " " + sides.get(1) + " " + sides.get(2);
+    }
 }
